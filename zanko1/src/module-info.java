5b730d41bd15d55d16d32ee06999e0b052a055d8
Yadaali3 <73434882+Yadaali3@users.noreply.github.com>
2020-10-26 16:35:54 +0300

    Update module-info.java

--- zanko1/src/module-info.java
+++ zanko1/src/module-info.java
@@ -1,2 +1,38 @@
-module zanko1 {
+import java.util.*;
+ class arr {
+	public static void main(String [] arg) {
+		
+		String yad="";
+		Scanner a = new Scanner(System.in);
+		Scanner b = new Scanner(System.in);
+		
+		System.out.print("zhmaray pitakany nawakat chanda ? ");
+		int cla = a.nextInt();
+		
+		String []ab=new String [cla];
+		
+		for(int k =0;k<ab.length;k++) {
+			System.out.print((k+1)+"m pity nawakat bnwsa"+k);
+			ab[k]=b.nextLine();
+			
+			
+			yad=yad+ab[k];
+			
+		}
+		System.out.println();
+		System.out.println("nawakat "+yad);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
+
